0953b8d033f2a78360d4ca87eb2dc609e1a8fc07
Mike Robinson <badtoyz@gmail.com>
2014-03-30 21:06:03 -0400

    plugins for oh-my-zsh and prezto

---
+++ README.md
@@ -0,0 +1,34 @@
+Textadept
+=========
+[Textadept](http://foicica.com/textadept/) A fast, minimalist, and remarkably extensible cross-platform text editor
+
+Custom alias's for textadept and textadept-curses   
+
+
+
+Aliases
+-------
+    - `ta` textadept-curses
+    - `taj` textadeptjit-curses
+    - `taedit`textadept-curses ~/.textadept/init.lua
+    - `taui` textadept
+    - `tauij` textadeptjit
+    - `tauiedit` textadept ~/.textadept/init.lua
+    - `tahelp` help output for textadept
+    
+            Usage: textadept [args] [filenames]
+
+            -e, --execute  1  Run Lua code.
+            -f, --force  0  Forces unique instance.
+            -h, --help   0  Shows this.
+            -n, --nosession  0  No session functionality.
+            -s, --session  1  Loads session on startup.
+            -u, --userhome   1  Sets alternate _USERHOME.
+
+            Textadept curses does not support the help switch
+
+Author
+------
+
+[Mike Robinson](https://github.com/badtoyz)
+
---
+++ init.zsh
@@ -0,0 +1,34 @@
+# ------------------------------------------------------------------------------
+#          FILE:  init.zsh
+#   DESCRIPTION:  Prezto module.
+#        AUTHOR:  Mike Robinson (badtoyz@gmail.com)
+#       VERSION:  1.0.0
+# ------------------------------------------------------------------------------
+# look for textadept-curses
+if [ -f '/usr/local/bin/textadept-curses' ];then
+    alias ta='textadept-curses'
+    alias taj='textadeptjit-curses'
+    alias taedit='textadept-curses ~/.textadept/init.lua'
+  else
+fi
+# look for textadept
+if [ -f '/usr/local/bin/textadept' ];then
+    alias taui='textadept'
+    alias tauij='textadeptjit'
+    alias tauiedit='textadept ~/.textadept/init.lua'
+  else
+fi
+# help
+function taHelp(){
+    echo "Usage: textadept [args] [filenames]"
+    echo ""
+    echo "    -e, --execute  1  Run Lua code."
+    echo "    -f, --force  0  Forces unique instance."
+    echo "    -h, --help   0  Shows this."
+    echo "    -n, --nosession  0  No session functionality."
+    echo "    -s, --session  1  Loads session on startup."
+    echo "    -u, --userhome   1  Sets alternate _USERHOME."
+    echo ""
+    echo "Textadept curses does not support the help switch"
+}
+alias tahelp=taHelp
